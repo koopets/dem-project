04fcebd2d3b1c1ae5323b587a6c55b1eebb71a70
vasili kuptsov <koopets@vasilis-MacBook-Pro.local>
2021-10-20 23:31:39 +0300

    Check your head, Somethings Got To Give

---
+++ gitTest/gitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by vasili kuptsov on 18.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
